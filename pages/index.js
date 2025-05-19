8d019bde3ad27a97798ab5b063365535225fda9d
ankki-hacker <pramanikankit08@gmail.com>
2025-05-19 08:00:47 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,26 @@
+// pages/index.js
+import Head from 'next/head';
 
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My Deployed Site</title>
+        <meta name="description" content="All root files deployed with this page" />
+      </Head>
+
+      <h1>Welcome to My Site</h1>
+      <p>This page ensures all root files are deployed:</p>
+      
+      <ul>
+        <li><code>.env</code> (used in build)</li>
+        <li><code>.eslintrc.json</code> (config)</li>
+        <li><code>next.config.js</code> (Next.js settings)</li>
+        <li>All other root files</li>
+      </ul>
+
+      {/* Example: Link to a public file */}
+      <a href="/resume.pdf">Download Resume (if placed in `public/`)</a>
+    </div>
+  );
+}
